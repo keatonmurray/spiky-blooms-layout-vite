ae2a5bf48914714574ee866fa731ca774e8c236f
Keaton Anthony Murray <murraykeaton422@gmail.com>
2024-10-03 22:58:21 -0400

    initial setup completed

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css';
+import '@fontsource/playfair-display'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  // You can manipulate the DOM or add features here if needed in the future
+});
